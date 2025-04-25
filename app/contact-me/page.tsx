297866c3cdd6ad911ef56ad63b2126b119ff6d06
Barry Fearon <barry.fearon@gamingmechanics.com>
2025-04-25 18:37:13 +0100

    added mobile nav

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -1,9 +1,24 @@
 "use client";
-import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactPage() {
-  const [state, handleSubmit, reset] = useForm("mjkyqplg");
+  const [state, submit, reset] = useForm("mjkyqplg");
+
+  if (state.submitting) {
+    return (
+      <main className="bg-gray-50 min-h-screen text-gray-900">
+        <div className="grid place-items-center max-w-2xl mx-auto h-screen">
+          <div className="self-end">
+            <h1 className="text-4xl font-bold mb-4 text-center">Sending...</h1>
+            <p className="text-gray-900">
+              Please wait while we send your message.
+            </p>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
   if (state.succeeded) {
     return (
       <main className="bg-gray-50 min-h-screen text-gray-900">
@@ -28,57 +43,61 @@ export default function ContactPage() {
   }
 
   return (
-    <main className="bg-gray-50 min-h-screen px-6 pt-30 pb-40 md:py-16 px-4 text-gray-900">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-4xl font-bold mb-10 text-center">Get in Touch</h1>
+    <main className="bg-gray-50 text-gray-900 min-h-screen grid px-4 ">
+      <div className="self-center">
+        <div className="max-w-2xl mx-auto">
+          <h1 className="text-4xl font-bold mb-10 text-center">Get in Touch</h1>
 
-        <form
-          onSubmit={handleSubmit}
-          className="space-y-6 bg-white p-8 rounded-lg shadow"
-        >
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium mb-1">
-              Your Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full border border-gray-300 rounded px-4 py-2 text-sm"
-              required
-            />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
-          </div>
-          <div>
-            <label htmlFor="message" className="block text-sm font-medium mb-1">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full border border-gray-300 rounded px-4 py-2 text-sm"
-              rows={5}
-              required
-            ></textarea>
-            <ValidationError
-              prefix="Message"
-              field="message"
-              errors={state.errors}
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={state.submitting}
-            className="bg-brand-01 text-white px-6 py-2 rounded hover:bg-brand-02 transition mt-4"
-            onClick={reset}
+          <form
+            onSubmit={submit}
+            className="space-y-6 bg-white p-8 rounded-lg shadow"
           >
-            Send Message
-          </button>
-        </form>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium mb-1">
+                Your Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full border border-gray-300 rounded px-4 py-2 text-sm"
+                required
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium mb-1"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full border border-gray-300 rounded px-4 py-2 text-sm"
+                rows={5}
+                required
+              ></textarea>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={state.submitting}
+              className="bg-brand-01 text-white px-6 py-2 rounded hover:bg-brand-02 transition mt-4"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,11 +8,11 @@ export default function Footer() {
       <div className="fixed bottom-0 left-0 right-0 bg-brand-02 z-10 shadow-sm">
         <div className="">
           <div className="grid grid-cols-2 gap-4 md:gap-16 p-4 max-w-4xl mx-auto text-center">
-            <div className="md:justify-self-end">
+            <div className="md:justify-self-end self-center">
               <p>
                 <a
                   href="mailto:barryfearon@gmail.com"
-                  className="text-white hover:text-brand-01 transition text-sm"
+                  className="text-white hover:text-gray-300 transition text-sm"
                 >
                   barryfearon@gmail.com
                 </a>
@@ -21,18 +21,18 @@ export default function Footer() {
               <p>
                 <a
                   href="tel:+447624452446"
-                  className="text-white hover:text-brand-01 transition text-sm"
+                  className="text-white hover:text-gray-300 transition text-sm"
                 >
                   +44 (0)7624 452446
                 </a>
               </p>
             </div>
-            <div className="md:justify-self-start">
+            <div className="md:justify-self-start self-center">
               {" "}
               <Link
                 href="/downloads/Barry-Fearon-Creative-Director-Project-Leader.pdf"
                 target="_blank"
-                className="cursor-pointer inline-block border border-white text-white px-4 py-2 rounded hover:bg-brand-01 hover:text-white transition"
+                className="cursor-pointer inline-block border border-white text-white px-4 py-2 rounded hover:bg-brand-01 hover:text-white transition text-sm"
               >
                 Download CV
               </Link>
